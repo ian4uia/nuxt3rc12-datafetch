90eb5a29a431304359ed3c370f3440c8af9dec9e
ian4uia <ian.jones@uia.co.uk>
2022-10-19 17:19:16 +0100

    Replicate issue

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>useFetch</h1>
+    <button @click="doRefresh()">Refresh</button><RouterLink to="pageTwo">Page 2</RouterLink>
+    <div v-if="pending">
+      <h2>Loading...</h2>
+    </div>
+    <div v-else>
+      <img :src="dogImage.message">
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { usePageStore } from '~~/stores/pageStore'
+  const pageStore = usePageStore()
+  const { data: dogImage, pending, refresh } = await useLazyAsyncData(
+    'dogImage',
+    () => $fetch('https://dog.ceo/api/breeds/image/random'),
+    { server: true }
+  )
+
+  if (pageStore.fromPage === 2) {
+    console.log('Another page load.')
+    refresh().then( ()=> {
+      console.log(dogImage.value.message)
+    })
+  } else {
+    console.log('First page load!')
+  }
+
+  const doRefresh = () => {
+    console.log('About to refresh')
+    refresh().then( ()=> {
+      console.log(dogImage.value.message)
+    })
+  }
+</script>
---
+++ pages/pageTwo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>useFetch - Page 2</h1>
+    <button @click="goBack()">Go Back</button>
+  </div>
+</template>
+
+<script setup>
+import { usePageStore } from '~~/stores/pageStore'
+const pageStore = usePageStore()
+const router = useRouter()
+if (true) {
+  pageStore.fromPage = 2
+}
+const goBack = () => {
+  router.push('/')
+}
+</script>
